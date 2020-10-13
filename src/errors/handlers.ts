f8caf626a56d5d661f8b7b04fda934f0d22157a0
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-10-13 18:02:02 -0300

    feat: add validation

--- src/errors/handlers.ts
+++ src/errors/handlers.ts
@@ -1,6 +1,21 @@
 import { ErrorRequestHandler, response } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  if (error instanceof ValidationError) {
+    const errors: ValidationErrors = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    return response.status(400).json({ message: 'Validation fails', errors });
+  }
+
   // eslint-disable-next-line no-console
   console.error(error);
 
