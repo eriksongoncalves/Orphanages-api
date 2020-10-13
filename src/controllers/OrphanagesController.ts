5eb9ed35df6f05331412f46596c32e7c598df129
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-10-13 17:45:30 -0300

    feat: add views

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,22 +2,27 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
 import Orphanage from '../models/Orphanage';
+import OrphanageView from '../views/orphanages_view';
 
 export default {
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return res.json(orphanages);
+    return res.json(OrphanageView.renderMany(orphanages));
   },
   async show(req: Request, res: Response) {
     const { id } = req.params;
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return res.json(orphanage);
+    return res.json(OrphanageView.render(orphanage));
   },
   async create(req: Request, res: Response) {
     const {
